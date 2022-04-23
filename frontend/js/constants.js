3379aced357a41feb32f9ed04a1f3243e58cb5a8
Clayham <103946652+Gnomereon@users.noreply.github.com>
2022-04-22 20:25:28 -0700

    commit

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
-const chain = 'rinkeby'; // rinkeby or polygon
+const contractAddress = "0x80d56F76DA6063aE8B11f2e7763e634aC140CD6B"; //Replace with your own contract address
+const chain = 'polygon'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to Gnomereon's Gnomes Passion Project";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The Gnomereons Gnomes NFT Project is a decentralized, open-source passion project inspired by our love of Gnomes. Each Gnomereon Gnome is truly one of a kind, as we all are.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
@@ -13,12 +13,12 @@ const p_presale_coming_soon = "We are working hard to launch the NFT Collection.
 const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
 const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
 const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
-const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
-const button_presale_coming_soon = "Get on the Whitelist";
-const button_presale_mint_whitelisted = "Mint Your Special NFT";
-const button_presale_mint_not_whitelisted = "Get on the Whitelist";
+const p_public_mint = "No whitelist needed. Public minting is now open! Claim your Gnomereon Gnome 🎉";
+const button_presale_coming_soon = "Join the Whitelist";
+const button_presale_mint_whitelisted = "Mint Your Unique Gnome NFT";
+const button_presale_mint_not_whitelisted = "Join the Whitelist";
 const button_presale_already_minted = "Join The Community";
-const button_public_mint = "Mint Your NFT";
+const button_public_mint = "Mint Your Unique Gnome NFT";
 const mint_failed = "Minting failed. 😢 Please try again.";
 
 const abi = [
